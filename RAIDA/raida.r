2fa20b8cb6fdecf276cc995255df9d422d0f3181
skosowsky <sakosows@ncsu.edu>
2015-06-04 16:46:24 -0400

    output code

--- RAIDA/raida.r
+++ RAIDA/raida.r
@@ -12,28 +12,37 @@ filenames<-list.files("C:\\Users\\Sean\\Documents\\R\\Data\\Metagenomic-Analysis
 for (i in 1:length(filenames)) assign(filenames[[i]], read.csv(filenames[[i]]))
 for (i in 1:length(filelist)){ 
 filelist[[i]]<-as.data.frame(read.csv(filelist[[i]]))}
+water<-c("PHYLLO09","PHYLLO10","PHYLLO11","PHYLLO13","PHYLLO15","PHYLLO21","PHYLLO23","PHYLLO26","PHYLLO27")
+drought<-c("PHYLLO12","PHYLLO14","PHYLLO16","PHYLLO22","PHYLLO24","PHYLLO25","PHYLLO28","PHYLLO29")
 
-
-
-n.list=list()
-for(i in 1:length(filelist)){
-n.list[[i]]<-dim(filelist[[i]])[2]}
-raida.list<-list()
-for(j in 1:length(filelist)){
-raida.list[[j]]<-raida(filelist[[j]],n.list[[j]])}
-
-for(i in 1:7){
-for(j in 1:dim(filelist[[i]])[2])
-if(colnames(filelist[[i]])[-1]==rownames(filelist[[5]])[j]&filelist[[5]][j,3]=="watered")
-data.water=matrix(nrows=dim(filelist[[i]])[1],ncols=count(filelist[[5]][,3])[2,2],byrow=T)}
-
-for(j in 1:dim(filelist[[1]])[2]){
-data.water=matrix(nrow=dim(filelist[[1]])[1],ncol=count(filelist[[5]][,3])[2,2],byrow=T)
-if(colnames(filelist[[1]])[-1][j]==as.vector(filelist[[5]]$Sample_ID)[j] & as.vector(filelist[[5]]$treatment)[j]=="watered"){
-data.water=filelist[[1]][,j]}
+filecount<-c(1,2,3,4,6,7)
+waterlist<-list()
+for(i in 1:6){
+waterlist[[i]]<-as.data.frame(cbind(filelist[[filecount[i]]][,1],filelist[[filecount[i]]][,which(colnames(filelist[[filecount[i]]])[-1]==as.vector(filelist[[5]]$Sample_ID) & as.vector(filelist[[5]]$treatment)=="watered")+1]))
 }
 
-data.water=matrix(nrow=dim(filelist[[1]])[1],ncol=count(filelist[[5]][,3])[2,2])
-data.water<-as.data.frame(cbind(filelist[[1]][,which(colnames(filelist[[1]])[-1]==as.vector(filelist[[5]]$Sample_ID) & as.vector(filelist[[5]]$treatment)=="watered")]))
+droughtlist<-list()
+for(i in 1:6){
+droughtlist[[i]]<-as.data.frame(cbind(filelist[[filecount[i]]][,which(colnames(filelist[[filecount[i]]])[-1]==as.vector(filelist[[5]]$Sample_ID) & as.vector(filelist[[5]]$treatment)=="drought")+1]))
+}
 
-cat("$",as.vector(filelist[[5]][which(colnames(filelist[[1]])[-1]==as.vector(filelist[[5]]$Sample_ID) & as.vector(filelist[[5]]$treatment)=="watered"),1])[1],sep="")
+ordereddata<-list()
+for(i in 1:6){
+ordereddata[[i]]<-as.data.frame(cbind(waterlist[[i]],droughtlist[[i]]))}
+
+watercount<-vector(length=6)
+for(i in 1:6){
+watercount[i]<-dim(waterlist[[i]])[2]}
+
+droughtcount<-vector(length=6)
+for(i in 1:6){
+droughtcount[i]<-dim(droughtlist[[i]])[2]}
+
+
+for(i in 1:6){
+rownames(ordereddata[[i]])<-ordereddata[[i]][,1]
+ordereddata[[i]]<-ordereddata[[i]][-1]}
+
+raida.list<-list()
+for(j in 1:6){
+raida.list[[j]]<-raida(ordereddata[[j]],c(9,8))}
